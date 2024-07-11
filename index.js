e8bbd12cfb9011cb34a7dc04fec2cdee68802843
shivam <shivprishi@gmail.com>
2024-07-12 00:04:23 +0530

    Home Page UI Initial

--- index.js
+++ index.js
@@ -10,14 +10,76 @@ const app = express();
 //middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(helmet());
+// app.use(helmet());
 
 
 //by default ejs will look into the views folder
 app.set('view engine', 'ejs');
 
+//static files serving
+app.use("/assets", express.static("views/assets"));
+app.use("/css", express.static("views/css"));
+
 app.get("/", (req, res) => {
-    res.render("index");
+    const data = {
+        carousel_cards: [
+            {
+                img_src: "/assets/college-overview.jpg",
+                title: "Overview",
+                description: "Get all round information of the college before joining.",
+            },
+            {
+                img_src: "/assets/connect_student.png",
+                title: "Connect",
+                description: "Chat with the Almuni of the College.",
+            },
+            {
+                img_src: "/assets/college_ambassdor.jfif",
+                title: "Ambassdor",
+                description: "Be the Representative of Your College and help other students to find their dream college.",
+            },
+            {
+                img_src: "/assets/need_help.jpg",
+                title: "Need Help?",
+                description: "Talk to us for clearing any Doubts!",
+            },
+        ],
+
+        college_cards: [
+            {
+                img_src: "/assets/iiit_bh.jfif",
+                title: "IIIT Bhagalpur",
+                description: "It is one of the National Institute of Importance located in the city of Bhagalpur.",
+            },
+            {
+                img_src: "/assets/iiit_bh.jfif",
+                title: "IIIT Bhagalpur",
+                description: "It is one of the National Institute of Importance located in the city of Bhagalpur.",
+            },
+            {
+                img_src: "/assets/iiit_bh.jfif",
+                title: "IIIT Bhagalpur",
+                description: "It is one of the National Institute of Importance located in the city of Bhagalpur.",
+            },
+            {
+                img_src: "/assets/iiit_bh.jfif",
+                title: "IIIT Bhagalpur",
+                description: "It is one of the National Institute of Importance located in the city of Bhagalpur.",
+            },
+            {
+                img_src: "/assets/iiit_bh.jfif",
+                title: "IIIT Bhagalpur",
+                description: "It is one of the National Institute of Importance located in the city of Bhagalpur.",
+            },
+            {
+                img_src: "/assets/iiit_bh.jfif",
+                title: "IIIT Bhagalpur",
+                description: "It is one of the National Institute of Importance located in the city of Bhagalpur.",
+            },
+        ],
+    };
+
+    res.render("index", data);
 });
 
 
